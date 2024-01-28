a7188863fc76baff1cc371f2e8358df86d4590a5
jaseemuddin <jaseemuddinn@gmail.com>
2024-01-28 12:10:46 +0530

    features added

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Hero from './components/Hero/Hero'
+import About from './components/About/About'
+import Skills from './components/Skills/Skills'
+import Project from './components/Project/Project'
+import Contact from './components/contact/contact'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <h1 className='text-center'>Jaseem</h1> */}
       <Hero />
+      <About />
+      <Skills />
+      <Project />
+      <Contact />
     </>
   )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,11 +5,11 @@ function Hero() {
   return (
     <div className="bg-cover h-screen bg-center md:h-auto lg:h-screen" style={{ backgroundImage: `url(${backgroundImg})` }}>
       <div className='pt-[250px] lg:pt-[280px] lg:ml-[280px] flex flex-col text-center lg:text-left'>
-      <h1 className='font-semibold text-white text-xl lg:text-7xl md:text-4xl'>Hi,</h1>
-      <h2 className='font-bold text-white text-2xl lg:text-7xl md:text-4xl'>I'm Jaseemuddin Naseem</h2>
-      <h3 className='text-white font-medium text-lg lg:text-2xl'>& I am a Web Developer!</h3>
+        <h1 className='font-semibold text-white text-xl lg:text-7xl md:text-4xl'>Hi,</h1>
+        <h2 className='font-bold text-white text-2xl lg:text-7xl md:text-4xl'>I'm Jaseemuddin Naseem</h2>
+        <h3 className='text-white font-medium text-lg lg:text-2xl'>& I am a Web Developer!</h3>
       </div>
-   </div>
+    </div>
   )
 }
 
