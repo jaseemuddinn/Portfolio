5c24e0daadc485e43ae11fd31a9f760dd3ca14b8
jaseemuddin <jaseemuddinn@gmail.com>
2024-02-05 00:48:13 +0530

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from './components/About/About'
 // import Skills from './components/Skills/Skills'
 import PointerCircle from './components/Pointer/PointerCircle'
 import Project from './components/Project/Project'
-import Contact from './components/Contact/Contact.jsx'
+import Contact from './components/Contact/Contact'
 
 function App() {
   return (
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,14 +1,22 @@
 import mailIcon from '../../assets/mail.svg'
 import hoverMailIcon from '../../assets/afterMail.svg'
+import { useState } from 'react'
 function Contact() {
+
+    const [isHovered, setIsHovered] = useState(false);
     return (
         <div className='bg-[#413E40]'>
             <div>
                 <h1 className='text-white pt-10 font-bold text-center text-2xl md:text-5xl lg:text-7xl'>Want To Talk?<span className='text-gray-200 text-lg font-normal'>About any project</span></h1>
                 <div className='py-5 flex justify-center items-center'>
-                    <a href="mailto:njaseemuddin@gmail.com" className='flex justify-center items-center group hover:italic'>
-                        <img className='p-2 transition duration-300 ease-in-out transform group-hover:hidden' src={mailIcon} alt="mailIcon" />
-                        <img className='p-2 hidden transition duration-300 ease-in-out transform group-hover:inline-block' src={hoverMailIcon} alt="hoverMailIcon" />
+                    <a href="mailto:njaseemuddin@gmail.com" className='flex justify-center items-center group hover:italic'
+                        onMouseEnter={() => setIsHovered(true)}
+                        onMouseLeave={() => setIsHovered(false)}>
+                        {isHovered ? (
+                            <img className='p-2 transition duration-300 ease-in-out transform' src={hoverMailIcon} alt="hoverMailIcon" />
+                        ) : (
+                            <img className='p-2 transition duration-300 ease-in-out transform' src={mailIcon} alt="mailIcon" />
+                        )}
                         <p className='text-white'>
                             Just E-mail
                         </p>
