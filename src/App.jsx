5593db2cfc8ce1f8441efe377428d929444e2afe
jaseemuddin <jaseemuddinn@gmail.com>
2024-02-05 00:30:55 +0530

    big fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import './App.css'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
 // import Skills from './components/Skills/Skills'
-import Contact from './components/Contact/Contact'
 import PointerCircle from './components/Pointer/PointerCircle'
 import Project from './components/Project/Project'
+import Contact from './components/Contact/Contact'
 
 function App() {
   return (
