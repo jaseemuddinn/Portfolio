c9e31b59c7ac0d9f03ce8c930751cb5e6ab42ddc
jaseemuddin <jaseemuddinn@gmail.com>
2024-02-05 00:39:13 +0530

    bug fixes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
             <div>
                 <h1 className='text-white pt-10 font-bold text-center text-2xl md:text-5xl lg:text-7xl'>Want To Talk?<span className='text-gray-200 text-lg font-normal'>About any project</span></h1>
                 <div className='py-5 flex justify-center items-center'>
-                    <a href="mailto:njaseemuddin@gmail.com" class='flex justify-center items-center group hover:italic'>
+                    <a href="mailto:njaseemuddin@gmail.com" className='flex justify-center items-center group hover:italic'>
                         <img className='p-2 transition duration-300 ease-in-out transform group-hover:hidden' src={mailIcon} alt="mailIcon" />
                         <img className='p-2 hidden transition duration-300 ease-in-out transform group-hover:inline-block' src={hoverMailIcon} alt="hoverMailIcon" />
                         <p className='text-white'>
