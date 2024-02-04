35599a9a1fa2d64e82f95123cd5fd93dfb5d7c60
jaseemuddin <jaseemuddinn@gmail.com>
2024-02-04 21:52:52 +0530

    features added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-
+import mailIcon from '../../assets/mail.svg'
+import hoverMailIcon from '../../assets/afterMail.svg'
+// import './Contact.css'
 function Contact() {
-  return (
-    <div className='h-lvh bg-[#413E40]'>
-        <div>
-            <h1 className='text-white pt-10 font-bold text-center text-3xl md:text-5xl lg:text-7xl'>Want To Talk?<span className='text-gray-200 text-lg font-normal'>About any project</span></h1>
+    return (
+        <div className='bg-[#413E40]'>
+            <div>
+                <h1 className='text-white pt-10 font-bold text-center text-3xl md:text-5xl lg:text-7xl'>Want To Talk?<span className='text-gray-200 text-lg font-normal'>About any project</span></h1>
+                <div class='py-5 flex justify-center items-center'>
+                    <a href="mailto:njaseemuddin@gmail.com" class='flex justify-center items-center group hover:italic'>
+                        <img class='p-2 transition duration-300 ease-in-out transform group-hover:hidden' src={mailIcon} alt="mailIcon" />
+                        <img class='p-2 hidden transition duration-300 ease-in-out transform group-hover:inline-block' src={hoverMailIcon} alt="hoverMailIcon" />
+                        <p class='text-white'>
+                            Just E-mail
+                        </p>
+                    </a>
+                </div>
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Contact
