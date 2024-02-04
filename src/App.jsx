5ba1dbc065b611b9c14570f6184fce0f9d2b14bb
jaseemuddin <jaseemuddinn@gmail.com>
2024-02-04 22:21:59 +0530

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
-import Skills from './components/Skills/Skills'
+// import Skills from './components/Skills/Skills'
 import Contact from './components/contact/contact'
 import PointerCircle from './components/Pointer/PointerCircle'
 import Project from './components/Project/Project'
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
     return (
         <div className='bg-[#413E40]'>
             <div>
-                <h1 className='text-white pt-10 font-bold text-center text-3xl md:text-5xl lg:text-7xl'>Want To Talk?<span className='text-gray-200 text-lg font-normal'>About any project</span></h1>
+                <h1 className='text-white pt-10 font-bold text-center text-2xl md:text-5xl lg:text-7xl'>Want To Talk?<span className='text-gray-200 text-lg font-normal'>About any project</span></h1>
                 <div class='py-5 flex justify-center items-center'>
                     <a href="mailto:njaseemuddin@gmail.com" class='flex justify-center items-center group hover:italic'>
                         <img class='p-2 transition duration-300 ease-in-out transform group-hover:hidden' src={mailIcon} alt="mailIcon" />
