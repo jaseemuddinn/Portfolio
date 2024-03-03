268b5c5811fd52a237e9f0f0607517d0b38a5981
jaseemuddin <jaseemuddinn@gmail.com>
2024-03-03 13:21:10 +0530

    contact added

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 function Info() {
-  return (
-    <div>Contact me at: njaseemuddin@gmail.com</div>
-  )
+    return (
+        <div className='bg-[#413E40]'>
+            <div>
+                <h1 className='text-white pt-10 font-bold text-center text-2xl md:text-5xl lg:text-7xl'>
+                    Want to talk? <span className='text-gray-200 text-lg font-normal'>About any project</span>
+                </h1>
+            </div>
+        </div>
+    )
 }
 
 export default Info
