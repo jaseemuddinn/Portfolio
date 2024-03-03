04532e9e8413cc964bed82689b7cffe3e8835101
jaseemuddin <jaseemuddinn@gmail.com>
2024-03-03 13:24:24 +0530

    mail added

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -7,6 +7,13 @@ function Info() {
                 <h1 className='text-white pt-10 font-bold text-center text-2xl md:text-5xl lg:text-7xl'>
                     Want to talk? <span className='text-gray-200 text-lg font-normal'>About any project</span>
                 </h1>
+                <div className='py-5 felx justify-center items-center'>
+                    <a href="mailto:njaseemudin@gmail.com" className='flex justify-center items-center hover:italic'>
+                        <p className='text-white'>
+                            Just E-mail
+                        </p>
+                    </a>
+                </div>
             </div>
         </div>
     )
